3b16a092481af19401a6066629a0153828b439ee
jose carlos goncalves <josecarlosgoncalves@outlook.pt>
2025-09-30 11:13:41 +0100

    continuar

---
+++ ucs/uc00245-desenvolver_algoritmos/exercicios/SOMAR.md
@@ -0,0 +1,14 @@
+# Desenvolver algoritmos 
+#
+## =====================
+## Jose Carlos Gonçalves
+## 25/09/2025
+## Somar dois numeros
+## =====================
+
+- inicio
+   - ler numeroUm
+   - ler numerodois
+   - resultado = numeroUm + numeroDois
+   - imprimir resultado
+- fim
---
+++ ucs/uc00245-desenvolver_algoritmos/exercicios/somar.py
@@ -0,0 +1,20 @@
+# Exercício 001: Faça um Programa que peça dois números e imprima a soma.
+# 
+# Autor: Jose Carlos Gonçalves
+# Data: 25/09/2025
+# Versão: 1.0
+# Status: Concluído
+# Licença: MIT
+# GitHub:
+#   
+ 
+print ("Somar dos numeros")
+print ("------------------")
+a = int(input("Digite o primeiro numero: "))    
+b = int(input("Digite o segundo numero: "))    
+soma = a + b 
+print ("A soma dos numeros é: ", soma) 
+print ("Fim do programa")
+
+
+
